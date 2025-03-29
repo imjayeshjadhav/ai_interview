27e2abbdd5f1dbcc211e1498ee8f72e8f950122f
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-03-29 18:15:34 +0530

    HomePage basic UI Completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,16 @@
 import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const RootLayout = ({children} :{children  :ReactNode}) => {
   return (
-    <div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex in-checked: gap-2 '>
+          <Image src="/logo.svg" alt='logo' height={32} width={38} />
+          <h2 className='text-primary-100'>PerepWise</h2>
+        </Link>
+      </nav>
       {children}
     </div>
   )
